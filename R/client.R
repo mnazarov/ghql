5ae9102e3ee9887789eb762ea4e0a70904a720dc
Maxim Nazarov <maxim.nazarov@openanalytics.eu>
2022-02-11 01:41:10 +0100

    allow underscores in variables
    Modified regex to match the spec - https://spec.graphql.org/October2021/#Letter

--- R/client.R
+++ R/client.R
@@ -247,7 +247,7 @@ GraphqlClient <- R6::R6Class(
 
   private = list(
     # @field .var_regex variable regexp 
-    .var_regex = '\\$([[:alnum:]]+)',
+    .var_regex = '\\$([_A-Za-z][_0-9A-Za-z]*)',
 
     # @description rewrite query if there is fragments, leave equal otherwise
     # @param x (character) a query, of class `query` or `fragment`
